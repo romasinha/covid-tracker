0a25c73ac1783e4602997119c76edfe63a600dab
romasinha <sinha.roma99@gmail.com>
2020-07-24 03:14:11 +0530

    api

---
+++ tracker/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Cards, Chart, CountryPicker} from './components';
+import {fetchData} from './api';
+import styles from './App.module.css';
+
+
+
+class App extends React.Component{
+
+  async componentDidMount(){
+    const data = await fetchData()
+    console.log(data);
+  }
+  render(){
+    return(
+      <div className = {styles.container}>
+        <Cards/>
+        <Chart/>
+        <CountryPicker/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
