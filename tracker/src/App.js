76ac5781f8ffc752389255b2b565a27357199afa
romasinha <sinha.roma99@gmail.com>
2020-07-28 02:14:38 +0530

    countries

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -22,9 +22,9 @@ class App extends React.Component{
     return(
       <div className = {styles.container}>
         <Cards data ={data}/>
-        <Chart/>
         <CountryPicker/>
-
+        <Chart/>
+        
       </div>
     );
   }
